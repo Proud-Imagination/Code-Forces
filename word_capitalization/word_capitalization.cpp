cd70210e5ca92284ed2a8573dcd122d7ca812cba
Vignesh Vembar <vighnesh.vembaar@gmail.com>
2023-06-04 23:10:55 +0300

    shorter code
    using strings, learnt that strings are mutable in cpp

--- word_capitalization/word_capitalization.cpp
+++ word_capitalization/word_capitalization.cpp
@@ -4,14 +4,10 @@
 using namespace std;
 
 int main () {
-    char s[1000];
-    memset(s, ' ', 1000);
-
+    string s;
     cin >> s;
 
-    if (islower(s[0])) {
-        s[0] = toupper(s[0]);
-    }
+    toupper(s[0]);
 
     cout << s;
 }
