f866424b53e9d1ec9ff2a84bd0d64fc4672a12f7
Vignesh Vembar <vighnesh.vembaar@gmail.com>
2023-06-04 22:24:43 +0300

    fix runtime error
    using char array instead of string

--- word/word.cpp
+++ word/word.cpp
@@ -3,12 +3,12 @@
 using namespace std;
 
 int main() {
-    string s;
+    char s[100];
     cin >> s;
 
     int lower = 0;
     int upper = 0;
-    for (int i = 0; i < s.length(); i++) {
+    for (int i = 0; i < 100; i++) {
         if (islower(s[i])) {
             lower++;
         }
