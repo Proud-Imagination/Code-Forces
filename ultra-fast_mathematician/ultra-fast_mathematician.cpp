eaf95ab0fef4b945f55ab786fe1212e467ea0b4c
Vignesh Vembar <vighnesh.vembaar@gmail.com>
2023-06-07 21:02:46 +0300

    fixes runtime error
    uses simpler method

--- ultra-fast_mathematician/ultra-fast_mathematician.cpp
+++ ultra-fast_mathematician/ultra-fast_mathematician.cpp
@@ -3,29 +3,19 @@
 
 using namespace std;
 
-string show_binary(unsigned int u, int num) 
-{ 
-  string output = "";
-  int t = pow(2, num);
-
-  for(t; t > 0; t = t / 2)           
-      if(u >= t) {                
-          u -= t;
-          output += "1";              
-      }
-      else {
-          output += "0";               
-      }
-      
-  return output ;                    
-}
-
 int main() {
     string s1, s2;
     cin >> s1 >> s2;
-    int n1 = stoi(s1, 0 ,2);
-    int n2 = stoi(s2, 0 ,2);
-    int output = n1 ^ n2;
+    string output{};
+    
+    for (int i = 0; i < s1.length(); i++) {
+        if (s1[i] == s2[i]) {
+            output += '0';
+        }
+        else {
+            output += '1';
+        }
+    }
 
-    cout << show_binary(output, s1.length() - 1);
+    cout << output;
 }
